d221c8f41a0562a7d20d8b6eeb3f0dc471cf6b8f
Sean Hanlon <shanlon800@gmail.com>
2018-01-24 22:27:34 -0500

    Added state and an onclick listener to the button. Updated the README.

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -4,7 +4,6 @@ import ChartContainer from './containers/ChartContainer'
 const App = props => {
   return(
     <div>
-      <h1>Hello from App</h1>
       <ChartContainer />
     </div>
   )
--- app/javascript/containers/ChartContainer.js
+++ app/javascript/containers/ChartContainer.js
@@ -4,13 +4,27 @@ class ChartContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      clickCounter: 0
+    }
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
+  handleOnClick() {
+    if (this.state.clickCounter < 10) {
+      let newCount = this.state.clickCounter + 1
+      this.setState({clickCounter: newCount})
+    } else {
+      this.setState({clickCounter: 0})
     }
   }
 
   render() {
+    console.log(this.state.clickCounter)
     return(
-      <h2>Chart Container</h2>
+      <div>
+        <h2>Chart Container</h2>
+        <button onClick={this.handleOnClick}>FILL THE DONUT</button>
+      </div>
     )
   }
 }
